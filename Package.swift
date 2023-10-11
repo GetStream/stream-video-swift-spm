16a7ae72740e013c8354c32f92bca8ae538f7b46
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2023-10-11 12:27:13 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.5
+
+import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "StreamVideo",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13), .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "StreamVideo",
+            targets: ["StreamVideo"]
+        ),
+        .library(
+            name: "StreamVideoSwiftUI",
+            targets: ["StreamVideoSwiftUI"]
+        ),
+        .library(
+            name: "StreamVideoUIKit",
+            targets: ["StreamVideoUIKit"]
+        )
+    ],
+    targets: [
+        .binaryTarget(name: "StreamVideo",
+                      url: "https://github.com/GetStream/stream-video-swift/releases/download/0.4.0/StreamVideo.zip",
+                      checksum: ""),
+        .binaryTarget(name: "StreamVideoSwiftUI",
+                      url: "https://github.com/GetStream/stream-video-swift/releases/download/0.4.0/StreamVideoSwiftUI.zip",
+                      checksum: ""),
+        .binaryTarget(name: "StreamVideoUIKit",
+                      url: "https://github.com/GetStream/stream-video-swift/releases/download/0.4.0/StreamVideoUIKit.zip",
+                      checksum: "")
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Stream Video
+
+### Please refer to the main repo for more information: [stream-video-swift](https://github.com/GetStream/stream-video-swift/)
+
+This repository just serves as the hosting for our Package.swift pointing to precompiled XCFrameworks.
